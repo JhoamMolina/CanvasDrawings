c70f17aa1033278f29cfee4c2174881eb2d15441
JhoamMolina <molina_504@hotmail.com>
2023-09-26 23:26:18 -0500

    Finished drawing circles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import "font-awesome/css/font-awesome.min.css";
+import { useState } from "react";
 import "./App.css";
 import Canvas from "./Components/Canvas";
 
 function App() {
+  const [shapeType, setShapeType] = useState("rectangle"); // or 'circle'
+
   return (
     <div className="App">
+      <div style={{ marginBottom: "10px", display: "flex" }}>
+        <button
+          onClick={() => setShapeType("rectangle")}
+          className="button rectangle-button"
+        >
+          <i className="fa fa-square-o fa-2x icon" aria-hidden="true"></i> Draw
+          Rectangle
+        </button>
+        <button
+          onClick={() => setShapeType("circle")}
+          className="button circle-button"
+        >
+          <i className="fa fa-circle-o fa-2x icon" aria-hidden="true"></i> Draw
+          Circle
+        </button>
+      </div>
       <Canvas
+        shapeType={shapeType}
         width={700}
         height={500}
         imageUrl="https://i.pinimg.com/1200x/00/2d/57/002d5714c44f88a16c1f0bdfa97ca05e.jpg"
--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useRef } from "react";
 import { useOnDraw } from "./Hooks";
 
 const Canvas = (props) => {
-  const { width, height, imageUrl } = props;
+  const { width, height, imageUrl, shapeType } = props;
   const { setCanvasRef, canvasRef, lastPointRef, prevPointRef } =
     useOnDraw(onDraw);
 
@@ -11,15 +11,44 @@ const Canvas = (props) => {
   const imgRef = useRef(new Image());
   imgRef.current.src = imageUrl;
 
+  console.log();
+
+  function drawCircle(start, end, ctx, color) {
+    const minX = Math.min(start.x, end.x);
+    const minY = Math.min(start.y, end.y);
+    const maxX = Math.max(start.x, end.x);
+    const maxY = Math.max(start.y, end.y);
+
+    // Calculate side to form a square
+    const side = Math.max(maxX - minX, maxY - minY);
+
+    // Adjust min coordinates to form a square
+    const adjustedMinX = start.x < end.x ? minX : maxX - side;
+    const adjustedMinY = start.y < end.y ? minY : maxY - side;
+
+    // Calculate the center and the radius of the circle
+    const centerX = adjustedMinX + side / 2;
+    const centerY = adjustedMinY + side / 2;
+    const radius = side / 2;
+
+    // Draw the circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+  }
+
   const redraw = useCallback(() => {
     const ctx = canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, width, height);
     ctx.drawImage(imgRef.current, 0, 0, width, height);
-    console.log(rectanglesRef);
-
-    rectanglesRef.current.forEach((rect) =>
-      drawRectangle(rect.start, rect.end, ctx, "black")
-    );
+    rectanglesRef.current.forEach((shape) => {
+      if (shape.type === "rectangle") {
+        drawRectangle(shape.start, shape.end, ctx, "black");
+      } else if (shape.type === "circle") {
+        drawCircle(shape.start, shape.end, ctx, "black");
+      }
+    });
   }, [canvasRef, width, height]);
 
   useEffect(() => {
@@ -29,12 +58,12 @@ const Canvas = (props) => {
 
   function onDraw(ctx, point, prevPoint) {
     if (!prevPoint) return;
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     redraw();
-    rectanglesRef.current.forEach((rect) =>
-      drawRectangle(rect.start, rect.end, ctx, "black")
-    ); // Redraw all rectangles
-    drawRectangle(prevPoint, point, ctx, "black");
+    if (shapeType === "rectangle") {
+      drawRectangle(prevPoint, point, ctx, "black");
+    } else if (shapeType === "circle") {
+      drawCircle(prevPoint, point, ctx, "black"); // prevPoint is center, point is current mouse position
+    }
   }
 
   function drawRectangle(start, end, ctx, color) {
@@ -50,8 +79,12 @@ const Canvas = (props) => {
 
   function onMouseUp() {
     if (prevPointRef.current && lastPointRef.current) {
-      const rect = { start: prevPointRef.current, end: lastPointRef.current };
-      rectanglesRef.current.push(rect);
+      const shape = {
+        type: shapeType,
+        start: prevPointRef.current,
+        end: lastPointRef.current,
+      };
+      rectanglesRef.current.push(shape); // This array will now store both rectangles and circles.
     }
   }
 
